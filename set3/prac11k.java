33eb426d236be16e18aad3c5842364af923ea6be
DharoharChhaya <23dcs016@charusat.edu.in>
2024-08-05 21:34:40 +0530

    Create prac11k.java

---
+++ set3/prac11k.java
@@ -0,0 +1,9 @@
+class poundconvert
+{
+      public static void main(String a[])
+	{
+                    int x=Integer.parseInt(a[0]);
+		int y=x*100;
+		System.out.println("The value is rupees is: "+y);
+	}
+}
